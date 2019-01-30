c06c5e6273e4ee50416fe8bc47fc3a4fb70331ec
ernestonr1 <ernstras@gmail.com>
2019-01-30 12:24:27 +0100

    Add project files.

---
+++ EC-Ecom2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EC_Ecom2.MvcApplication" Language="C#" %>
---
+++ EC-Ecom2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EC_Ecom2.Startup))]
+namespace EC_Ecom2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
